9c983f4b32971c99b2e381a6c5775978aca5fdb0
pmoraa522-gif <pmoraa522@utb.edu.ec>
2025-11-15 12:21:43 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'ecuador-gold': '#F9A825',
+        'ecuador-red': '#D50000',
+      }
+    },
+  },
+  plugins: [],
+}
